03f987e3dc6662de91f7e013a355143c9505d0ff
R-M-D-S <zama@royalmcgrady.co.za>
2024-07-04 14:55:19 +0200

    main

--- app.py
+++ app.py
@@ -47,19 +47,6 @@ def get_image_base64(image_raw):
 
 def main():
 
-    st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-
     # --- Page Config ---
     st.set_page_config(
         page_title="Thuto The Tutor",
