4f128509e6ba5dcfc5c4374ef6d546410a3a2f9d
R-M-D-S <zama@royalmcgrady.co.za>
2024-07-04 14:53:46 +0200

    menu

--- app.py
+++ app.py
@@ -47,10 +47,18 @@ def get_image_base64(image_raw):
 
 def main():
 
-    hide_github_icon = """
-    #MainMenu {visibility: hidden;}
+    st.markdown(
     """
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
 
     # --- Page Config ---
     st.set_page_config(
